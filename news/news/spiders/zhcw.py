1a8e4da84117ca2c111e3e85b44ea5a0ddb791ec
Sebastian-github-2020 <zaksvisitor@gmail.com>
2022-06-14 11:31:12 +0800

    取消遵守robo协议

--- news/news/spiders/zhcw.py
+++ news/news/spiders/zhcw.py
@@ -2,12 +2,18 @@ import scrapy
 
 
 class ZhcwSpider(scrapy.Spider):
-    # 爬虫的名字  
+    # 爬虫的名字
     name = 'zhcw'
     # 允许访问的域名
     allowed_domains = ['zhwc.com']
     # 起始url地址
     start_urls = ['http://zhwc.com/']
 
+
     def parse(self, response):
+        """
+        运行了 起始url后执行的方法
+        :param response:
+        :return:
+        """
         pass
